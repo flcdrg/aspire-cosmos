23b5ae41c40d81645492a5351b30b64367660df5
David Gardiner <david@gardinerfamily.au>
2026-05-11 21:20:28 +0930

    not working with console

--- CosmosClientApp/Program.cs
+++ CosmosClientApp/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.Net;
+using Microsoft.Azure.Cosmos;
+
+Console.WriteLine("Hello, World!");
+
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__cosmos")
+    ?? throw new InvalidOperationException("Connection string 'ConnectionStrings__cosmos' was not provided.");
+
+Console.WriteLine("Using cosmos account connection string from Aspire reference.");
+
+using CosmosClient client = new(
+    accountEndpoint: "https://localhost:8081/",
+    authKeyOrResourceToken: "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw=="
+);
+
+Database database = await client.CreateDatabaseIfNotExistsAsync(
+    id: "cosmicworks",
+    throughput: 400
+);
--- aspire-cosmos.AppHost/AppHost.cs
+++ aspire-cosmos.AppHost/AppHost.cs
@@ -1,3 +1,14 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var cosmos = builder.AddAzureCosmosDB("cosmos")
+    .RunAsEmulator(emulator =>
+    {
+        //emulator.WithImage("");
+    });
+
+var app = builder.AddProject<Projects.CosmosClientApp>("app")
+    .WaitFor(cosmos)
+    .WithReference(cosmos)
+    ;
+
 builder.Build().Run();
